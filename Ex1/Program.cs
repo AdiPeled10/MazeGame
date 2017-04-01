baffe5ececd94def8dff24c49e171d0bfb975953
Adi Peled <adi.peled1210@gmail.com>
2017-04-01 21:44:50 +0300

    Linked the dlls to the project. Also,

--- Ex1/Program.cs
+++ Ex1/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MazeLib;
 
 namespace Ex1
 {
@@ -10,6 +11,7 @@ namespace Ex1
     {
         static void Main(string[] args)
         {
+            Maze m = new Maze(5, 5);
             Console.WriteLine("Hello World!!");
             Console.ReadKey();
         }

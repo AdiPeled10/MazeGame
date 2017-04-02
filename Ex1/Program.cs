d44d6dba1cf0ce41857bc6aedc0ebfa672a7c70e
Adi Peled <adi.peled1210@gmail.com>
2017-04-02 12:49:02 +0300

    Created the Class Libaray "SearchAlgorithmsLib" with all the interfaces and class from "AP2 - design". Code is partial and not function.

--- Ex1/Program.cs
+++ Ex1/Program.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using MazeLib;
+using MazeGeneratorLib;
 
 namespace Ex1
 {
@@ -11,10 +12,11 @@ namespace Ex1
     {
         static void Main(string[] args)
         {
-            Maze m = new Maze(5, 5);
-            MazeGeneratorLib.DFSMazeGenerator dfsMaze = new MazeGeneratorLib.DFSMazeGenerator();
-            Console.WriteLine("Hello World!!");
-            Console.ReadKey();
+            DFSMazeGenerator dfsMaze = new DFSMazeGenerator();
+            Maze m = dfsMaze.Generate(10, 5);
+            m = new Maze(5, 5);
+            Console.WriteLine(m[1, 1]);
+            //Console.ReadKey();
         }
     }
 }
---
+++ SearchAlgorithmsLib/State.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SearchAlgorithmsLib
+{
+    public class State<T>
+    {
+        private T state; // the state represented by a string
+        private double cost; // cost to reach this state (set by a setter)
+        private State<T> cameFrom; // the state we came from to this state (setter)
+
+        public State(T state) // CTOR
+        {
+            this.state = state;
+        }
+
+        public override bool Equals(object obj) // we override Object's Equals method
+        {
+            return state.Equals((obj as State<T>).state);
+        }
+
+        public override int GetHashCode()
+        {
+            /*
+             * TODO change it if we'll want to use the state as a dictionary key
+             * and we'll want to know the difference between two States that have
+             * the same "state" but different "cost" or "cameFrom".
+             */
+            return state.GetHashCode();
+        }
+    }
+}

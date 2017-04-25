44aadd6013135f4c6bb84f3019acdff8078d6439
Lior <liormoshe0909@gmail.com>
2017-04-26 01:07:09 +0300

    Added the XAML design for the main menu of the game and the popup window for the single player button, the next step will be to implement the single player game which means to implement by the principles of MVVM.Started adding the IPropertyChanged interface in the new ViewModel project and the View is written with XAML in the GUIClient project.

---
+++ GUIClient/MainWindow.xaml.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace GUIClient
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        public void SinglePlayerClick(object sender,RoutedEventArgs args) 
+        {
+            Window popupWindow = new Window();
+            //Set width and height of the window to be medium sized.
+            popupWindow.Width = 400;
+            popupWindow.Height = 400;
+            //Create StackPanel layout to hold Name,Rows,Cols of generated maze.
+            StackPanel myStackPanel = new StackPanel();
+            myStackPanel.Children.Add(new MazeInformationLayout());
+            //myStackPanel.Children.Add(GenerateTextBox("Name:"));
+            //myStackPanel.Children.Add(GenerateTextBox("Number of Rows:"));
+            //myStackPanel.Children.Add(GenerateTextBox("Number of Columns:"));
+            //Add Ok button.
+            myStackPanel.Children.Add(new Button { Width = 40,Content = "OK",
+                Height = 40, Margin = new Thickness(50)});
+            popupWindow.Content = myStackPanel;
+
+            popupWindow.Show();
+        }
+
+        /// <summary>
+        /// This function will generate a label and a TextBox next to it in
+        /// a StackPanel layout.
+        /// </summary>
+        /// <param name="nameOfField">
+        /// The name of the label.
+        /// </param>
+        /// <returns></returns>
+        private StackPanel GenerateTextBox(string nameOfField)
+        {
+            StackPanel myStackPanel = new StackPanel { Orientation = Orientation.Vertical };
+            myStackPanel.Children.Add(new Label { Content = nameOfField });
+            myStackPanel.Children.Add(new TextBox());
+            return myStackPanel;
+        }
+
+        private void OKButtonClick(object sender,RoutedEventArgs args)
+        {
+
+        }
+
+        private void MultiPlayerClick(object sender,RoutedEventArgs args)
+        {
+           
+        }
+
+        private void SettingsClick(object sender,RoutedEventArgs args)
+        {
+
+        }
+
+    }
+}
---
+++ GUIClient/MazeInformationLayout.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace GUIClient
+{
+    /// <summary>
+    /// Interaction logic for MazeInformationLayout.xaml
+    /// </summary>
+    public partial class MazeInformationLayout : UserControl
+    {
+        public MazeInformationLayout()
+        {
+            InitializeComponent();
+        }
+
+        public string NameBox
+        {
+            get { return nameTextBox.Text; }
+        }
+
+        public int Rows
+        {
+            get { return Int32.Parse(rowsTextBox.Text); }
+        }
+
+        public int Cols
+        {
+            get { return Int32.Parse(columnsTextBox.Text); }
+        }
+    }
+}
